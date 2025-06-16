1b1dea2c8196efc7029dd1f4f9534bfec4781409
Rajani.Kushwaha <Rajani.Kushwaha@IVLH-LP619.INFOVISIONLABS.COM>
2025-06-16 22:42:16 +0530

    file handling

--- classactivity/src/com_day6/FileHandlingBuffered.java
+++ classactivity/src/com_day6/FileHandlingBuffered.java
@@ -1,27 +1,56 @@
 package com_day6;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FileHandlingBuffered {
 	public static void main(String [] args)
 	{
-		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in)))
+		try(FileReader fr=new FileReader("C:\\Users\\Rajani.kushwaha\\git\\repository3\\classactivity\\src\\com_day6\\RajaniNote.txt"))
 		{
-			System.out.println("Enter file to be read");
-			int letter=br.read();
-			while(br.ready())
+			int letter=fr.read();
+			while(fr.ready())
 			{
 				System.out.println((char)letter);
+				letter=fr.read();
 			}
-			System.out.println();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in)))
+		{
+			System.out.println("Enter file to be read"+ br.readLine());
+
 		}
 		catch(IOException e){
 			
 				System.out.println(e.getMessage());
 			}
 		
-	}
+		
+		try(BufferedReader br=new BufferedReader(new FileReader( "C:\\\\Users\\\\Rajani.kushwaha\\\\git\\\\repository3\\\\classactivity\\\\src\\\\com_day6\\\\RajaniNote.txt")))
+				{
+					
+					while(br.ready())
+					{
+						System.out.println(br.readLine());
+						
+					}
+				}
+					
+					catch(IOException e)
+					{
+						System.out.println(e.getMessage());
+					}
+					
+				
+		
+				}
+
 
 }
+
